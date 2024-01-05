21ce6c3976412942dcf43f1f48c89d8e3b384616
Tim <timhofmann01@gmail.com>
2024-01-05 02:15:01 +0100

    favicon

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,45 +1,7 @@
 import 'https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js';
 
-export function select(el, all = false) {
-	el = el.trim();
-	if (all) {
-		return [...document.querySelectorAll(el)];
-	} else {
-		return document.querySelector(el);
-	}
-}
-
-/**
- * Easy event listener function
- */
-export function on(type, el, listener, all = false) {
-	let selectEl = select(el, all);
-	if (selectEl) {
-		if (all) {
-			selectEl.forEach((e) => e.addEventListener(type, listener));
-		} else {
-			selectEl.addEventListener(type, listener);
-		}
-	}
-}
-
-/**
- * Easy on scroll event listener
- */
-export function onscroll(el, listener) {
-	el.addEventListener('scroll', listener);
-}
-
-/**
- * Add a typing animation to an element using [typed.js](https://github.com/mattboldt/typed.js)
- * To define the strings to be typed, add a data-typed-items attribute to the element.
- * @example <p><span class="typed" data-typed-items="First Item, Second Item, Third Item, ..."></span></p>
- * @param {*} typeSpeed
- * @param {*} backSpeed
- * @param {*} backDelay
- */
 export function typeEffect(typeSpeed = 100, backSpeed = 50, backDelay = 2000) {
-	const typed = select('.typed');
+	const typed = document.querySelector('.typed');
 	if (typed) {
 		let typed_strings = typed.getAttribute('data-typed-items');
 		typed_strings = typed_strings.split(',');
