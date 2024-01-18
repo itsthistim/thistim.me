61249fe37dd08f8cb31534b2a7b606e14a309e41
Tim <timhofmann01@gmail.com>
2024-01-18 17:15:25 +0100

    change build format to get rid of trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	trailingSlash: 'never'
+	trailingSlash: 'never',
+	build: {
+		format: 'file'
+	}
 });
