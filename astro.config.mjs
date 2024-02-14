15dd8082e0b972ba3cf813f4a83e6bc103a9e6e8
Tim <timhofmann01@gmail.com>
2024-02-15 00:38:43 +0100

    * consider sidebar when centering * update astro-icon * fix redundant size strings for project cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.thistim.me',
-	trailingSlash: 'never'
+  site: 'https://www.thistim.me',
+  trailingSlash: 'never',
+  integrations: [icon()]
 });
